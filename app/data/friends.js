264ec06a3fca584cb19127ed365d5680206e731d
Corey Walsh <matthias61984@hotmail.com>
2018-11-08 23:20:02 -0800

    boilerplate

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+var chefs = [
+    { "name":"Swedish Chef",
+        "photo":"https://vignette.wikia.nocookie.net/muppet/images/4/41/Swedish-chef.jpg/revision/latest?cb=20111104173207",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+];
