380751a56e663beccb54c144531128a3e228ab6d
Corey Walsh <matthias61984@hotmail.com>
2018-11-17 21:42:10 -0800

    WTF

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,23 +1,4 @@
 var friends = require("../data/friends.js");
-console.log(friends);
-
-var findBestFriend = function(arr, newFriend) {
-  var bestFriend;
-  var lowestDiff = 50;
-  for (i=0; i < arr.length; i++) {
-      var diff = 0;
-      for (j=0; j < arr[i].scores.length; j++) {
-          diff += Math.abs((arr[i].scores[j])-(newFriend[i]));
-      }
-      if (diff < lowestDiff) {
-          bestFriend = arr[i];
-          lowestDiff = diff;
-      }
-      console.log(diff); 
-  }
-  console.log("Your best friend is: " + bestFriend.name);
-  return bestFriend;
-}
 
 module.exports = function(app) {
 
@@ -27,9 +8,28 @@ module.exports = function(app) {
 
   app.post("/api/friends", function(req, res) {
     var newFriend = req.body;
-    var bestFriend = findBestFriend(friends, newFriend);
+    function findBestFriend(newFriend) {
+      var bestFriend;
+      var lowestDiff = 50;
+      for (i=0; i < friends.length; i++) {
+        var diff = 0;
+        for (j=0; j < friends[i].scores.length; j++) {
+            diff += Math.abs((friends[i].scores[j])-(newFriend.scores[j]));
+        }
+        if (diff < lowestDiff) {
+            bestFriend = friends[i];
+            lowestDiff = diff;
+        }
+        console.log(diff); 
+      }
+      console.log("Your best friend is: " + bestFriend.name);
+      return bestFriend;
+    }
+    findBestFriend()
+      .then(function(){
+        res.json(bestFriend);  
+      });
     friends.push(newFriend);
-    res.json(bestFriend);
   });
 
   app.post("/api/clear", function(req, res) {
