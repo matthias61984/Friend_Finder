ec8a6e4416475b128241d99523904533a2809a25
Corey Walsh <matthias61984@hotmail.com>
2018-11-19 18:25:09 -0800

    fixed

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,25 +8,28 @@ module.exports = function(app) {
 
   app.post("/api/friends", function(req, res) {
     var newFriend = req.body;
-    function findBestFriend(newFriend) {
-      var bestFriend;
-      var lowestDiff = 50;
-      for (i=0; i < friends.length; i++) {
-        var diff = 0;
-        for (j=0; j < friends[i].scores.length; j++) {
+    function findBestFriend() {
+      return new Promise((resolve,reject)=>{
+        var bestFriend;
+        var lowestDiff = 50;
+        for (i=0; i < friends.length; i++) {
+          var diff = 0;
+          for (j=0; j < friends[i].scores.length; j++) {
+            console.log(newFriend, friends[i].scores[j])
             diff += Math.abs((friends[i].scores[j])-(newFriend.scores[j]));
-        }
-        if (diff < lowestDiff) {
+          }
+          if (diff < lowestDiff) {
             bestFriend = friends[i];
             lowestDiff = diff;
+          }
+          console.log(diff); 
         }
-        console.log(diff); 
-      }
-      console.log("Your best friend is: " + bestFriend.name);
-      return bestFriend;
+        console.log("Your best friend is: " + bestFriend.name);
+        resolve(bestFriend);
+      })
     }
     findBestFriend()
-      .then(function(){
+      .then(function(bestFriend){
         res.json(bestFriend);  
       });
     friends.push(newFriend);
